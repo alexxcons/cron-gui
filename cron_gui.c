51a9166d5a091150410a1b5e2f6902fe42995f96
schwinn <schwinn@schwinn-desktop>
2016-08-20 23:44:36 +0200

    replaced glade entries for jobs

--- cron_gui.c
+++ cron_gui.c
@@ -32,8 +32,8 @@ const char * simpleTimingValues[] = {
 };
 const int SIMPLE_TIMING_VALUES_SIZE	= 6;
 
-GtkSizeGroup * sizeGroupLineNumbers = NULL;
-GtkSizeGroup * sizeGroupTimePickerBox = NULL;
+static GtkSizeGroup * sizeGroupLineNumbers = NULL;
+static GtkSizeGroup * sizeGroupTimePickerBox = NULL;
 
 void initSizeGroups()
 {
@@ -41,12 +41,13 @@ void initSizeGroups()
 	sizeGroupTimePickerBox = gtk_size_group_new (GTK_SIZE_GROUP_HORIZONTAL);
 }
 
-void fillFragmentLineNumber(GtkLabel *lineNumberLabel)
+void addLineNumber(GtkWidget *lineBox)
 {
 	int length = snprintf( NULL, 0, "%d", lineNumberGUI );
 	char* lineNumberStr = malloc( length + 1 );
 	snprintf( lineNumberStr, length + 1, "%d", lineNumberGUI );
-	gtk_label_set_text(lineNumberLabel,lineNumberStr);
+	GtkWidget *lineNumberLabel = gtk_label_new (lineNumberStr);
+	gtk_container_add (GTK_CONTAINER (lineBox), lineNumberLabel);
 	free(lineNumberStr);
 	lineNumberGUI++;
 	gtk_size_group_add_widget (sizeGroupLineNumbers,lineNumberLabel);
@@ -62,7 +63,6 @@ void activate_main_gui(GtkApplication *app, const char* fileToLoad)
 	g_signal_connect (window, "destroy", G_CALLBACK (on_main_window_destroy), NULL);
 	main_window = GTK_WINDOW(window);
 
-	//gtk_window_set_title (GTK_WINDOW (window), "Window");
 	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
 	GtkWidget *main_box = gtk_box_new (GTK_ORIENTATION_VERTICAL,0);
@@ -89,84 +89,78 @@ void activate_main_gui(GtkApplication *app, const char* fileToLoad)
 	gtk_main();
 }
 
-void addComment(GtkWidget *mainTable, const char *comment)
+void addCommentOrVariable(GtkWidget *mainTable, const char *text)
 {
-	GtkBuilder *builder = gtk_builder_new();
-	gtk_builder_add_from_file (builder, "main_window.glade", NULL);
-	GtkWidget *fragment = GTK_WIDGET(gtk_builder_get_object(builder, "comment"));
-	GtkWidget *textbox = gtk_builder_get_object (builder,"comment_textbox");
-	gtk_entry_set_text(textbox,comment);
-	GtkWidget *lineNumberLabel = gtk_builder_get_object (builder,"comment_lineNumber");
-	fillFragmentLineNumber(GTK_LABEL(lineNumberLabel));
-	gtk_builder_connect_signals(builder, NULL);
-	gtk_container_add(GTK_CONTAINER(mainTable),fragment);
-	g_object_unref(builder);
-}
+	GtkWidget *box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,0);
+	gtk_container_add (GTK_CONTAINER (mainTable), box);
 
-void addVariable(GtkWidget *mainTable, const char *text)
-{
-	GtkBuilder *builder = gtk_builder_new();
-	gtk_builder_add_from_file (builder, "main_window.glade", NULL);
-	GtkWidget *fragment = GTK_WIDGET(gtk_builder_get_object(builder, "variable"));
-	GtkWidget *textbox = gtk_builder_get_object (builder,"variable_textbox");
+	addLineNumber(box);
+
+	GtkWidget *textbox = gtk_entry_new ();
+	gtk_container_add (GTK_CONTAINER (box), textbox);
 	gtk_entry_set_text(textbox,text);
-	GtkWidget *lineNumberLabel = gtk_builder_get_object (builder,"variable_lineNumber");
-	fillFragmentLineNumber(GTK_LABEL(lineNumberLabel));
-	gtk_builder_connect_signals(builder, NULL);
-	gtk_container_add(GTK_CONTAINER(mainTable),fragment);
-	g_object_unref(builder);
+	gtk_entry_set_has_frame (textbox,FALSE);
+	gtk_widget_set_hexpand (textbox,TRUE);
+
+	gtk_widget_show_all (box);
 }
 
 void addSimpleCronJob(GtkWidget *mainTable, const char *simpleSelector, const char *command)
 {
-	GtkBuilder *builder = gtk_builder_new();
-	gtk_builder_add_from_file (builder, "main_window.glade", NULL);
-	GtkWidget *fragment = GTK_WIDGET(gtk_builder_get_object(builder, "cronjob.simple"));
-	GtkWidget *commandBox = gtk_builder_get_object (builder,"simple_command");
-	gtk_entry_set_text(commandBox,command);
-	GtkWidget *timePickerBox = gtk_builder_get_object (builder,"simple_timePicker");
-	gtk_size_group_add_widget (sizeGroupTimePickerBox,timePickerBox);
-	GtkWidget *choiceBox = gtk_builder_get_object (builder,"simple_choice");
+	GtkWidget *box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,0);
+	gtk_container_add (GTK_CONTAINER (mainTable), box);
+
+	addLineNumber(box);
+
+	GtkWidget *timePicker = gtk_combo_box_text_new ();
+	gtk_container_add (GTK_CONTAINER (box), timePicker);
+	gtk_size_group_add_widget (sizeGroupTimePickerBox,timePicker);
 	int i;
 	for( i = 0; i < SIMPLE_TIMING_VALUES_SIZE ;i++)
 	{
-		gtk_combo_box_text_append_text(choiceBox,simpleTimingValues[i]);
+		gtk_combo_box_text_append_text(timePicker,simpleTimingValues[i]);
 		if(strcmp(simpleTimingValues[i],simpleSelector) == 0)
 		{
-			gtk_combo_box_set_active (choiceBox,i);
+			gtk_combo_box_set_active (timePicker,i);
 		}
 	}
-	GtkWidget *lineNumberLabel = gtk_builder_get_object (builder,"simple_lineNumber");
-	fillFragmentLineNumber(GTK_LABEL(lineNumberLabel));
-	gtk_builder_connect_signals(builder, NULL);
-	gtk_container_add(GTK_CONTAINER(mainTable),fragment);
-	g_object_unref(builder);
+
+	GtkWidget *commandBox = gtk_entry_new();
+	gtk_container_add (GTK_CONTAINER (box), commandBox);
+	gtk_entry_set_text(commandBox,command);
+	gtk_widget_set_hexpand (commandBox,TRUE);
+	gtk_widget_show_all (box);
+}
+
+void addTimeSelectorButton(GtkWidget *buttonBox, int type, const char* value )
+{
+	GtkWidget *button = gtk_button_new_with_label (value);
+	gtk_container_add (GTK_CONTAINER (buttonBox), button);
+	g_signal_connect (button, "clicked", G_CALLBACK (on_time_selector_pressed), type);
 }
 
 void addAdvancedCronJob(GtkWidget *mainTable, const char *minute, const char *hour, const char *dom, const char *month, const char *dow, const char *command)
 {
-	GtkBuilder *builder = gtk_builder_new();
-	gtk_builder_add_from_file (builder, "main_window.glade", NULL);
-	GtkWidget *fragment = GTK_WIDGET(gtk_builder_get_object(builder, "cronjob.advanced"));
-	GtkWidget *commandBox = gtk_builder_get_object (builder,"advanced_command");
+	GtkWidget *box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,0);
+	gtk_container_add (GTK_CONTAINER (mainTable), box);
+
+	addLineNumber(box);
+
+	GtkWidget *buttons = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,0);
+	gtk_container_add (GTK_CONTAINER (box), buttons);
+	gtk_size_group_add_widget (sizeGroupTimePickerBox,buttons);
+
+	addTimeSelectorButton(buttons,MINUTE,minute);
+	addTimeSelectorButton(buttons,HOUR,hour);
+	addTimeSelectorButton(buttons,DOM,dom);
+	addTimeSelectorButton(buttons,MONTH,month);
+	addTimeSelectorButton(buttons,DOW,dow);
+
+	GtkWidget *commandBox = gtk_entry_new();
+	gtk_container_add (GTK_CONTAINER (box), commandBox);
 	gtk_entry_set_text(commandBox,command);
-	GtkWidget *minuteBox = gtk_builder_get_object (builder,"advanced_minute");
-	gtk_button_set_label (minuteBox,minute);
-	GtkWidget *hourBox = gtk_builder_get_object (builder,"advanced_hour");
-	gtk_button_set_label (hourBox,hour);
-	GtkWidget *domBox = gtk_builder_get_object (builder,"advanced_dom");
-	gtk_button_set_label (domBox,dom);
-	GtkWidget *monthBox = gtk_builder_get_object (builder,"advanced_month");
-	gtk_button_set_label (monthBox,month);
-	GtkWidget *dowBox = gtk_builder_get_object (builder,"advanced_dow");
-	gtk_button_set_label (dowBox,dow);
-	GtkWidget *lineNumberLabel = gtk_builder_get_object (builder,"advanced_lineNumber");
-	fillFragmentLineNumber(GTK_LABEL(lineNumberLabel));
-	GtkWidget *timePickerBox = gtk_builder_get_object (builder,"advanced_timePicker");
-	gtk_size_group_add_widget (sizeGroupTimePickerBox,timePickerBox);
-	gtk_builder_connect_signals(builder, NULL);
-	gtk_container_add(GTK_CONTAINER(mainTable),fragment);
-	g_object_unref(builder);
+	gtk_widget_set_hexpand (commandBox,TRUE);
+	gtk_widget_show_all (box);
 }
 
 void openWizardFromFile(char* fileName, char* wizardName)
@@ -185,44 +179,13 @@ void on_insert_line_activate(GtkWidget *button)
 	openWizardFromFile("newLine.glade","newLine");
 }
 
-// called when window is closed
 void on_main_window_destroy()
 {
-	printf("debug");
     gtk_main_quit();
 }
 
-void on_loadWizard_pressed(GtkWidget *button)
+void on_time_selector_pressed(GtkWidget *button, wizardType type)
 {
+	runWizard( type, main_window, button);
 	//printf("data: %i",data);
-}
-
-void on_cancel_pressed(GtkWidget *button, GtkWidget *wizardWindow)
-{
-	gtk_widget_destroy(wizardWindow);
-}
-
-void on_minute_pressed(GtkWidget *button)
-{
-	runWizard( MINUTE, main_window, button);
-}
-
-void on_hour_pressed(GtkWidget *button)
-{
-	runWizard( HOUR, main_window, button);
-}
-
-void on_dom_pressed(GtkWidget *button)
-{
-	runWizard( DOM, main_window, button);
-}
-
-void on_month_pressed(GtkWidget *button)
-{
-	runWizard( MONTH, main_window, button);
-}
-
-void on_dow_pressed(GtkWidget *button)
-{
-	runWizard( DOW, main_window, button);
 }
--- cron_gui.h
+++ cron_gui.h
@@ -17,13 +17,13 @@
 #define CRON_GUI_H
 
 #include <gtk/gtk.h>
+#include "wizard.h"
 
 GtkWindow *main_window;
 
 void addSimpleCronJob(GtkWidget *mainTable, const char *simpleSelector, const char *command);
 void addAdvancedCronJob(GtkWidget *mainTable, const char *minute, const char *hour, const char *dom, const char *month, const char *dow, const char *command);
-void addComment(GtkWidget *mainTable, const char *comment);
-void addVariable(GtkWidget *mainTable, const char *text);
+void addCommentOrVariable(GtkWidget *mainTable, const char *text);
 
 void initSizeGroups();
 
@@ -32,15 +32,8 @@ void activate_main_gui(GtkApplication *app, const char* fileToLoad );
 // called when window is closed
 void on_main_window_destroy();
 
-void on_cancel_pressed(GtkWidget *button, GtkWidget *wizardWindow);
-
 void on_insert_line_activate(GtkWidget *button);
 
-void on_loadWizard_pressed(GtkWidget *button);
-void on_minute_pressed(GtkWidget *button);
-void on_hour_pressed(GtkWidget *button);
-void on_dom_pressed(GtkWidget *button);
-void on_month_pressed(GtkWidget *button);
-void on_dow_pressed(GtkWidget *button);
+void on_time_selector_pressed(GtkWidget *button, wizardType type);
 
 #endif //CRON_GUI_H
